1ca106e4200e9d92bacfbb2af9fa113b9eb3699b
Rishabh0411 <rishabh.sharma041108@gmail.com>
2024-04-13 23:54:24 +0530

    initial menu

--- menu.py
+++ menu.py
@@ -1,15 +1,35 @@
 import pygame
-import main
-main_menu = False
+
+surface = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Menu")
+surface.fill((64, 230, 108))
 
 
-def draw_menu():
-    pass
+start = pygame.image.load("resources/start_btn.jpeg").convert_alpha()
+exit = pygame.image.load("resources/download.png").convert_alpha()
+speed = pygame.image.load("resources/maxresdefault.jpg").convert_alpha()
 
+class Button():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
 
-def draw_game():
-    game = main.Game()
-    menu_btn = pygame.draw.rect(game.surface, 'light gray', [230, 450, 260, 40], 0, 5)
-    pygame.draw.rect(game.surface, 'dark gray', [230, 450, 260, 40], 5, 5)
-    text = pygame.font.render('Main Menu', True, 'black')
-    game.surface.blit(text, (245, 457))
+    def draw(self):
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+start_btn = Button(10, 35, start)
+exit_btn = Button(250, 50, exit)
+speed_btn = Button(300, 350, speed)
+    
+running = True
+
+while running:
+    start_btn.draw()
+    exit_btn.draw()
+    speed_btn.draw()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()    
+pygame.quit()    
